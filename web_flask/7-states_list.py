18fa51f745c6050d43a1b059f6695478581bced8
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-04-22 23:24:01 -0500

    Improve engines

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Python script that starts a Flask web application:
+- Uses storage for fetching data from the storage engine
+- /states_list: display a HTML page: (inside the tag BODY) 
+"""
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/states_list/')
+def states_list():
+    """
+    Function associated to the '/states_list/' path
+    """
+    return render_template('7-states_list.html',
+                           states=storage.all(State).values())
+
+
+if __name__ == "__main__":
+    app.run()
