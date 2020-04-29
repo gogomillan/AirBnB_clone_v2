f6f6f48f3c2c3ddd5e9d5948af44864d108789d4
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-04-28 21:00:34 -0500

    Fix db_store code error

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -76,7 +76,7 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         Session = scoped_session(sessionmaker(bind=self.__engine,
                                               expire_on_commit=False))
-        self.__session = Session()
+        self.__session = Session
 
     def delete(self, obj=None):
         """delete obj from db session
